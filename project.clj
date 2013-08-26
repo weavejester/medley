5503205bbb1d7db55fbae927adfa0c4817ed332a
James Reeves <jreeves@weavejester.com>
2013-08-26 14:27:52 +0100

    Added Codox plugin to project.clj file

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[codox "0.6.4"]])
