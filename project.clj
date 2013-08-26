9460825fb9f5e8ca5bc58b10831209f4732d4c34
James Reeves <jreeves@weavejester.com>
2013-08-26 14:37:30 +0100

    Added description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject medley "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A lightweight library of useful pure functions"
+  :url "https://github.com/weavejester/medley"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
