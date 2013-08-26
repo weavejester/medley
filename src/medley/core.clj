2aa18864d67f7033d6e724174ac3510f35072108
James Reeves <jreeves@weavejester.com>
2013-08-26 03:03:16 +0100

    Added initial collection of useful functions

--- src/medley/core.clj
+++ src/medley/core.clj
@@ -1,6 +1,37 @@
 (ns medley.core)
 
-(defn foo
-  "I don't do a whole lot."
+(defn find-first
+  "Finds the first item in a collection that matches a predicate."
+  [pred coll]
+  (first (filter pred coll)))
+
+(defn update
+  "Updates a value in a map with a function."
+  [m k f & args]
+  (assoc m k (apply f (m k) args)))
+
+(defn dissoc-in
+  "Dissociate a key in a nested assocative structure, where ks is a sequence
+  of keys."
+  [m ks]
+  (update-in m (butlast ks) dissoc (last ks)))
+
+(defn map-keys
+  "Maps a function over the keys of an associative collection."
+  [f coll]
+  (reduce-kv #(assoc %1 (f %2) %3) (empty coll) coll))
+
+(defn map-vals
+  "Maps a function over the values of an associative collection."
+  [f coll]
+  (reduce-kv #(assoc %1 %2 (f %3)) (empty coll) coll))
+
+(defn queue
+  "Creates an empty persistent queue, or one populated with a collection."
+  ([] clojure.lang.PersistentQueue/EMPTY)
+  ([coll] (into (queue) coll)))
+
+(defn queue?
+  "Returns true if x implements IPersistentQueue."
   [x]
-  (println x "Hello, World!"))
+  (instance? clojure.lang.PersistentQueue x))
--- test/medley/core_test.clj
+++ test/medley/core_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [medley.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-find-first
+  (is (= (find-first even? [7 3 3 2 8]) 2))
+  (is (nil? (find-first even? [7 3 3 7 3]))))
+
+(deftest test-update
+  (is (= (update {:a 5} :a + 2) {:a 7})))
+
+(deftest test-dissoc-in
+  (is (= (dissoc-in {:a {:b {:c 1}}} [:a :b :c])
+         {:a {:b {}}})))
+
+(deftest test-map-keys
+  (is (= (map-keys name {:a 1 :b 2})
+         {"a" 1 "b" 2})))
+
+(deftest test-map-vals
+  (is (= (map-vals inc {:a 1 :b 2})
+         {:a 2 :b 3})))
+
+(deftest test-queue
+  (testing "empty"
+    (is (instance? clojure.lang.PersistentQueue (queue)))
+    (is (empty? (queue))))
+  (testing "not empty"
+    (is (instance? clojure.lang.PersistentQueue (queue [1 2 3])))
+    (is (= (first (queue [1 2 3])) 1))))
+
+(deftest test-queue?
+  (is (queue? clojure.lang.PersistentQueue/EMPTY))
+  (is (not (queue? []))))
