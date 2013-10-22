8aee4e7e9c16deaf4b87adade8097f3aafb23a14
James Reeves <jreeves@weavejester.com>
2013-10-22 18:38:53 -0400

    Released 0.1.3

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ out of place in the core Clojure library.
 
 To install, add the following to your project `:dependencies`:
 
-    [medley "0.1.2"]
+    [medley "0.1.3"]
 
 ## Documentation
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject medley "0.1.2"
+(defproject medley "0.1.3"
   :description "A lightweight library of useful pure functions"
   :url "https://github.com/weavejester/medley"
   :license {:name "Eclipse Public License"
