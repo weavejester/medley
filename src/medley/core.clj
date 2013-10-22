9124dcc57ace3a465b1e8df7c1bc69f3cfc29e24
James Reeves <jreeves@weavejester.com>
2013-10-22 18:29:16 -0400

    Fixed some docstrings

--- src/medley/core.clj
+++ src/medley/core.clj
@@ -65,7 +65,7 @@
   ([coll] (into (queue) coll)))
 
 (defn queue?
-  "Returns true if x implements IPersistentQueue."
+  "Returns true if x implements clojure.lang.PersistentQueue."
   [x]
   (instance? clojure.lang.PersistentQueue x))
 
@@ -84,7 +84,7 @@
 (defn mapply
   "Applies a function f to the argument list formed by concatenating
   everything but the last element of args with the last element of
-  args.  This is useful for applying a function that accepts keyword
+  args. This is useful for applying a function that accepts keyword
   arguments to a map."
   [f & args]
   (apply f (apply concat (butlast args) (last args))))
