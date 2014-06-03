138bf4850bd4364e786c2c7009231aea0c9913d3
James Reeves <jreeves@weavejester.com>
2014-06-03 18:54:15 +0100

    Released 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject medley "0.2.0"
+(defproject medley "0.2.1"
   :description "A lightweight library of useful pure functions"
   :url "https://github.com/weavejester/medley"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[codox "0.8.7"]]
   :codox {:defaults {:doc/format :markdown}
-          :src-dir-uri "http://github.com/weavejester/medley/blob/0.2.0/"
+          :src-dir-uri "http://github.com/weavejester/medley/blob/0.2.1/"
           :src-linenum-anchor-prefix "L"}
   :profiles
   {:dev {:dependencies [[criterium "0.4.2"]]}})
