b7e09f5f9ad8b831cc5be1be7995197770a37280
James Reeves <jreeves@weavejester.com>
2015-01-08 15:22:30 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[codox "0.8.10"]
-            [lein-cljsbuild "1.0.3"]
-            [com.cemerick/clojurescript.test "0.3.1"]]
+            [lein-cljsbuild "1.0.4"]
+            [com.cemerick/clojurescript.test "0.3.3"]]
   :codox {:defaults {:doc/format :markdown}
           :sources ["target/generated/src"]
           :src-dir-uri "http://github.com/weavejester/medley/blob/0.5.3/"
@@ -20,7 +20,6 @@
     {:source-paths ["test"], :output-path "target/generated/test", :rules :cljs}]}
   :source-paths ["src" "target/generated/src"]
   :test-paths   ["test" "target/generated/test"]
-  :hooks [cljx.hooks]
   :cljsbuild
   {:builds
    [{:source-paths ["target/generated/src" "target/generated/test"]
@@ -28,13 +27,15 @@
    :test-commands {"unit-tests" ["phantomjs" :runner "target/main.js"]}}
   :aliases
   {"deploy"    ["with-profile" "-dev" "deploy"]
-   "test-cljs" ["do" ["cljx" "once"] ["cljsbuild" "test"]]
-   "test-all"  ["do" ["with-profile" "default:+1.6:+1.7" "test"]
-                     ["cljsbuild" "test"]]}
+   "test-cljs" ["do" ["cljx" "once"]
+                     ["with-profile" "+1.6" "cljsbuild" "test"]]
+   "test-all"  ["do" ["cljx" "once"]
+                     ["with-profile" "default:+1.6:+1.7" "test"]
+                     ["with-profile" "+1.6" "cljsbuild" "test"]]}
   :profiles
-  {:provided {:dependencies [[org.clojure/clojurescript "0.0-2234"]]}
-   :dev {:dependencies [[criterium "0.4.2"]]
+  {:provided {:dependencies [[org.clojure/clojurescript "0.0-2665"]]}
+   :dev {:dependencies [[criterium "0.4.3"]]
          :jvm-opts ^:replace {}
-         :plugins [[com.keminglabs/cljx "0.4.0"]]}
+         :plugins [[com.keminglabs/cljx "0.5.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}})
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}})
