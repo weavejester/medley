222b5f2aabb848194a5a611a025a5b1a478ba3d2
James Reeves <jreeves@weavejester.com>
2016-04-05 19:30:13 +0100

    Release 0.7.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject medley "0.7.3"
+(defproject medley "0.7.4"
   :description "A lightweight library of useful, mostly pure functions"
   :url "https://github.com/weavejester/medley"
   :license {:name "Eclipse Public License"
