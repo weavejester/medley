30c20f6cdf22007eca1d83ff11cc9d0dee75ba98
James Reeves <jreeves@weavejester.com>
2025-08-29 22:33:02 +0100

    Update Clojure 1.12 project profile

--- project.clj
+++ project.clj
@@ -31,4 +31,4 @@
          :jvm-opts ^:replace {}}
    :1.10 {:dependencies [[org.clojure/clojure "1.10.0"]]}
    :1.11 {:dependencies [[org.clojure/clojure "1.11.2"]]}
-   :1.12 {:dependencies [[org.clojure/clojure "1.12.0-alpha9"]]}})
+   :1.12 {:dependencies [[org.clojure/clojure "1.12.2"]]}})
